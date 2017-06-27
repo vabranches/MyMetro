22a4c617184e7eb75dd87c6847c393b0acd32808
vabranches <valter_abranches@yahoo.com.br>
2017-06-26 22:38:30 -0300

    Implementando o Stetho

--- app/src/main/java/br/com/teravalt/mymetro/API/RetrofitClient.java
+++ app/src/main/java/br/com/teravalt/mymetro/API/RetrofitClient.java
@@ -1,6 +1,9 @@
 package br.com.teravalt.mymetro.API;
 
 
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,8 +12,13 @@ public class RetrofitClient {
 
     public static Retrofit getClient(String baseURL){
         if (retrofit==null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addNetworkInterceptor(new StethoInterceptor())
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseURL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
